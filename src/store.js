1683b0557065c30f694388d072d0bc25b4e38f73
Christian Zeck <christian.zeck@gmail.com>
2018-11-06 13:56:23 +0200

    Added mutations and actions

--- src/store.js
+++ src/store.js
@@ -4,7 +4,17 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    count: 15
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  },
+  actions: {
+    incrementCount({ commit }) {
+      commit('increment')
+    }
+  }
 })
