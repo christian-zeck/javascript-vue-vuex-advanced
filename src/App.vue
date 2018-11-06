5e053613d550a56e9bdece750bffb077e816fa53
Christian Zeck <christian.zeck@gmail.com>
2018-11-06 14:25:49 +0200

    Added mutations and getters

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="app">
+    <div>
+      # of tasks done: {{ doneTodosCount }}
+      <ul>
+        <li v-for="todo in doneTodos" :key="todo.id">
+          {{ todo.text }}
+        </li>
+      </ul>
+    </div>
     <button @click="increment">Increment</button>
     {{ count }}
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
   methods: {
     increment() {
@@ -13,9 +22,8 @@ export default {
     }
   },
   computed: {
-    count() {
-      return this.$store.state.count
-    }
+    ...mapState(['count']),
+    ...mapGetters(['doneTodos', 'doneTodosCount'])
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    count: 15
+    count: 15,
+    todos: [
+      { id: 1, text: 'Task 1', done: true },
+      { id: 2, text: 'Task 2', done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    doneTodosCount: (state, getters) => {
+      return getters.doneTodos.length
+    }
   },
   mutations: {
     increment(state) {
